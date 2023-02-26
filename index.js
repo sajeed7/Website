b88ef18db99a03c120d7e2ad315ddb28bd84d886
sajeed7 <109805660+sajeed7@users.noreply.github.com>
2023-02-26 17:00:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*function fn(){
+    var age=40;
+var isy=(age>=30)&&(age>=50)
+console.log(isy)
+}
+fn(20);*/
+// var x=chjxch
+// console.log(!//x)
+// function add(){
+    // var a=200;
+    // var b=55;
+    // var c=a+b;
+    // return c;
+// }
+// console.log(add());
+function add(a,b){
+    var c=a+b;
+    return c;
+}
+add(20,20);
